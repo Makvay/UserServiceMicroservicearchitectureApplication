ba093e376875adc18bbc552f039748ba5055f4cb
Makvay <bojkov07@list.ru>
2025-10-15 22:58:42 +0800

    start

---
+++ src/main/java/org/example/userservicemicroservicearchitecture/UserServiceMicroservicearchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.example.userservicemicroservicearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceMicroservicearchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceMicroservicearchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/userservicemicroservicearchitecture/UserServiceMicroservicearchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.userservicemicroservicearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceMicroservicearchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
